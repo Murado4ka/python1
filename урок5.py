f446761d6cb6f4cc9502e337363fe3a3d3a50063
Murado4ka <murad.mirzazade03@gmail.com>
2026-05-24 14:31:26 +0700

    урок 6

--- урок5.py
+++ урок5.py
@@ -1,11 +1,41 @@
 import random
-sec_num = random.randint(1, 10)
-print("Я загадал число от 1 до десяти попробуй отгадать")
-guess = int(input("твоё число: "))
+print("=" * 40)
+print("||" + " " * 10 + "Угадай число 2.0" + " " * 10 + "||")
+print("=" * 40)
+print("\nДобро пожаловать в улучшенную версию класической игры!")
+print("\nМеню")
+print("-" * 30)
+print("1. Одиночная игра против компютера")
+print("2. Парная игра с другом")
+print("3. Выход")
+print("-" * 30)
 while True:
-    guess = int(input("твоё число: "))
-    if guess != sec_num:
-       print ("Ты угадал❤")
-       break
-    else:
-        print("нет не так❤")
+    choice = input("\nВыберите действие(1-3): ")
+    if choice != "1" and choice != "2" and choiсe != "3":
+        print("Ошибка. Введи число от 1 до 3. Попробуй еще раз.")
+        continue
+    if choice == "1":
+        print("Ты играешь с пк")
+        sec_num = random.randint(1, 100)
+        attempts = 0
+        max_attempts = None
+        limit_choice = input("Установить лимит попыток? да или нет? ")
+        if limit_choice == "да":
+            max_attempts = int(input("Введи количество попыток: "))
+        print("Компьютер загадал число от 1 до 100. Попробуй угадать!")
+        while True:
+            guess = int(input("Ваша догадка: "))
+            attempts += 1
+            if guess < sec_num:
+                print("Число больше")
+            elif guess > sec_num:
+                print("Число меньше")
+            else:
+                print("Поздравляю ты угадал!")
+                break
+            if max_attempts and attempts >= max_attempts:
+                print(f"Попытки закончились. Загаданное число = {sec_num}.")
+                break
+
+
+
